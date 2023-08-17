14cbf593de4d7e825aa3e37f00ef4b40f8df0576
Adetwelve <niyero10@gmail.com>
2023-08-17 21:11:12 +0100

    task 7: define a user function that implement a POST/users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
     Flask app
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -13,6 +15,20 @@ def index():
     """json payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """
+        register new player
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
